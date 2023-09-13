d1c44381541298eeac96359bfbb6d02ceab52da4
Rohit <rkumar8092378845@gmail.com>
2023-09-13 22:47:35 +0530

    Initial commit 13-09-2023

---
+++ src/main/java/com/rkumar0206/mymapigateway/MymApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.rkumar0206.mymapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MymApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MymApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rkumar0206/mymapigateway/MymApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rkumar0206.mymapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MymApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
