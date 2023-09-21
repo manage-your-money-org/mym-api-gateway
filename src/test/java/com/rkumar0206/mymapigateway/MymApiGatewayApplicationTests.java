10c1ced27e56bed5bfdd17d36b3f7bbe7889d66b
Rohit <rkumar8092378845@gmail.com>
2023-09-21 23:30:25 +0530

    - added mym-expense-service path
    #1

--- src/test/java/com/rkumar0206/mymapigateway/MymApiGatewayApplicationTests.java
+++ src/test/java/com/rkumar0206/mymapigateway/MymApiGatewayApplicationTests.java
@@ -3,11 +3,11 @@ package com.rkumar0206.mymapigateway;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MymApiGatewayApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class MymApiGatewayApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
