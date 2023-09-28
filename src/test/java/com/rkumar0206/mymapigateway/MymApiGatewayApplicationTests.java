ba15224baf4ee20e14e7254067a6c6a5e3fcfeee
Rohit <rkumar8092378845@gmail.com>
2023-09-28 12:54:15 +0530

    - remove CustomResponse.java
    #1

--- src/test/java/com/rkumar0206/mymapigateway/MymApiGatewayApplicationTests.java
+++ src/test/java/com/rkumar0206/mymapigateway/MymApiGatewayApplicationTests.java
@@ -1,8 +1,5 @@
 package com.rkumar0206.mymapigateway;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
 //@SpringBootTest
 //class MymApiGatewayApplicationTests {
 //
