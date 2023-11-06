0c5316bcbb9c7b6e817cfee6a3e62390c1ad1c4f
Rohit <rkumar8092378845@gmail.com>
2023-11-07 02:26:51 +0530

    - added logs in RabbitMQDynamicSetupServiceImpl.java - Solved some errors
    #4

--- src/main/java/com/rkumar0206/mymapigateway/service/RabbitMQDynamicSetupServiceImpl.java
+++ src/main/java/com/rkumar0206/mymapigateway/service/RabbitMQDynamicSetupServiceImpl.java
@@ -2,14 +2,14 @@ package com.rkumar0206.mymapigateway.service;
 
 import com.rkumar0206.mymapigateway.config.MymRabbitMQConfig;
 import com.rkumar0206.mymapigateway.models.*;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class RabbitMQDynamicSetupServiceImpl implements RabbitMQDynamicSetupService {
 
     private final MymRabbitMQConfig mymRabbitMQConfig;
@@ -27,7 +27,11 @@ public class RabbitMQDynamicSetupServiceImpl implements RabbitMQDynamicSetupServ
     @Override
     public void createBinding(CreateBinding createBinding, String queueName) {
 
-        String createBidingUrl = baseRabbitMqUrl + "/bindings/" + mymRabbitMQConfig.getVhost().getName() + "/e/" + mymRabbitMQConfig.getMymExchange().getName() + "/q/" + queueName;
+        String exchangeName = mymRabbitMQConfig.getMymExchange().getName();
+
+        log.info("Creating binding with: exchange = " + exchangeName + " and queue = " + queueName);
+
+        String createBidingUrl = baseRabbitMqUrl + "/bindings/" + mymRabbitMQConfig.getVhost().getName() + "/e/" + exchangeName + "/q/" + queueName;
 
         HttpEntity<CreateBinding> createBindingHttpEntity1 = new HttpEntity<>(
                 createBinding, getHttpHeader()
@@ -38,14 +42,17 @@ public class RabbitMQDynamicSetupServiceImpl implements RabbitMQDynamicSetupServ
         );
 
         if (createBindingResponse.getStatusCode().is2xxSuccessful()) {
-            System.out.println("Binding with routing key - " + createBinding.getRouting_key() + " created successfully");
+            log.info("Create binding response: " + createBindingResponse.getStatusCode());
+            log.info("Binding created successfully");
         } else {
-            System.err.println("Failed to create binding");
+            log.error("Failed to create binding with response code : " + createBindingResponse.getStatusCode() + " and message: " + createBindingResponse.getBody());
         }
     }
 
     @Override
     public void createQueue(String queueName, CreateQueue createQueue) {
+
+        log.info("Creating queue: " + queueName);
 
         String createQueueUrl = baseRabbitMqUrl + "/queues/" + mymRabbitMQConfig.getVhost().getName() + "/" + queueName;
 
@@ -58,14 +65,17 @@ public class RabbitMQDynamicSetupServiceImpl implements RabbitMQDynamicSetupServ
         );
 
         if (createQueueResponse.getStatusCode().is2xxSuccessful()) {
-            System.out.println("Queue creating successful");
+            log.info("Create queue response: " + createQueueResponse.getStatusCode());
+            log.info("Queue : " + queueName + ", created successfully.");
         } else {
-            System.err.println("Failed to create queue");
+            log.error("Failed to create queue with response code : " + createQueueResponse.getStatusCode() + " and message: " + createQueueResponse.getBody());
         }
     }
 
     @Override
     public void createExchange(CreateExchange createExchange) {
+
+        log.info("Creating exchange: " + mymRabbitMQConfig.getMymExchange().getName());
 
         String createExchangeUrl = baseRabbitMqUrl + "/exchanges/" + mymRabbitMQConfig.getVhost().getName() + "/" + mymRabbitMQConfig.getMymExchange().getName();
 
@@ -78,9 +88,12 @@ public class RabbitMQDynamicSetupServiceImpl implements RabbitMQDynamicSetupServ
         );
 
         if (createExchangeResponse.getStatusCode().is2xxSuccessful()) {
-            System.out.println("Exchange creating successful");
+
+            log.info("Create exchange response : " + createExchangeResponse.getStatusCode());
+            log.info("Exchange " + mymRabbitMQConfig.getMymExchange().getName() + ", created successfully");
+
         } else {
-            System.err.println("Failed to create exchange");
+            log.error("Failed to create exchange " + mymRabbitMQConfig.getMymExchange().getName() + " with response code : " + createExchangeResponse.getStatusCode() + " and message: " + createExchangeResponse.getBody());
         }
 
     }
@@ -88,23 +101,30 @@ public class RabbitMQDynamicSetupServiceImpl implements RabbitMQDynamicSetupServ
     @Override
     public void createTopicPermission(TopicPermission topicPermission) {
 
+        log.info("Creating topic permission : " + topicPermission.getExchange() + " for the user");
+
         String topicPermissionBaseUrl = baseRabbitMqUrl + "/topic-permissions/" + mymRabbitMQConfig.getVhost().getName() + "/" + mymRabbitMQConfig.getNewUser().getUsername();
 
         HttpEntity<TopicPermission> topicPermissionRequestEntity = new HttpEntity<>(topicPermission, getHttpHeader());
 
-        ResponseEntity<String> topicPermissionResponse1 = restTemplate.exchange(
+        ResponseEntity<String> topicPermissionResponse = restTemplate.exchange(
                 topicPermissionBaseUrl, HttpMethod.PUT, topicPermissionRequestEntity, String.class
         );
 
-        if (topicPermissionResponse1.getStatusCode().is2xxSuccessful()) {
-            System.out.println("topic permission - " + topicPermission.getExchange() + " granted successfully.");
+        if (topicPermissionResponse.getStatusCode().is2xxSuccessful()) {
+
+            log.info("Create topic permission response : " + topicPermissionResponse.getStatusCode());
+            log.info("topic permission - " + topicPermission.getExchange() + " granted successfully.");
         } else {
-            System.err.println("Failed to grant topic permission - " + topicPermission.getExchange());
+
+            log.error("Failed to grant topic permission - " + topicPermission.getExchange() + " with response code : " + topicPermissionResponse.getStatusCode() + " and message: " + topicPermissionResponse.getBody());
         }
     }
 
     @Override
     public void createVhost() {
+
+        log.info("Creating new Vhost: " + mymRabbitMQConfig.getVhost().getName());
 
         // Create new Vhost
         String createVHostBaseUrl = baseRabbitMqUrl + "/vhosts/" + mymRabbitMQConfig.getVhost().getName();
@@ -113,46 +133,78 @@ public class RabbitMQDynamicSetupServiceImpl implements RabbitMQDynamicSetupServ
         ResponseEntity<String> vhostResponse = restTemplate.exchange(createVHostBaseUrl, HttpMethod.PUT, vhostRequestEntity, String.class);
 
         if (vhostResponse.getStatusCode().is2xxSuccessful()) {
-            System.out.println("Vhost created successfully.");
+
+            log.info("Create Vhost response : " + vhostResponse.getStatusCode());
+            log.info("Vhost created successfully.");
         } else {
-            System.err.println("Failed to create vhost.");
+            log.error("Failed to create Vhost with response code : " + vhostResponse.getStatusCode() + " with message : " + vhostResponse.getBody());
         }
     }
 
     @Override
     public void createRabbitMqUser() {
 
+        log.info("Creating new rabbitMQ user.");
+
         // Create rabbitMQ user
         String userCreateUrl = baseRabbitMqUrl + "/users/" + mymRabbitMQConfig.getNewUser().getUsername();
+
+        log.info("createRabbitMqUser : url : " + userCreateUrl);
 
         HttpHeaders headers = new HttpHeaders();
         headers.setBasicAuth("guest", "guest");
 
         // Create a request entity with the user details
         HttpEntity<RabbitMQUser> createUserRequestEntity = new HttpEntity<>(new RabbitMQUser("administrator", mymRabbitMQConfig.getNewUser().getPassword()), headers);
-        ResponseEntity<String> createUserResponse = restTemplate.exchange(userCreateUrl, HttpMethod.PUT, createUserRequestEntity, String.class);
 
-        if (createUserResponse.getStatusCode().is2xxSuccessful()) {
-            System.out.println("RabbitMq User created successfully.");
-        } else {
-            System.err.println("Failed to create rabbitmq user.");
+        try {
+
+            ResponseEntity<String> createUserResponse = restTemplate.exchange(userCreateUrl, HttpMethod.PUT, createUserRequestEntity, String.class);
+
+            if (createUserResponse.getStatusCode().is2xxSuccessful()) {
+
+                log.info("createRabbitMqUser : response : " + createUserResponse.getStatusCode());
+                log.info("RabbitMq User created successfully.");
+            } else {
+                log.error("Failed to create rabbitmq user with response code : " + createUserResponse.getStatusCode() + " with message : " + createUserResponse.getBody());
+                System.err.println("Failed to create rabbitmq user.");
+            }
+
+        } catch (HttpClientErrorException e) {
+
+            if (e.getStatusCode() == HttpStatus.UNAUTHORIZED) {
+                log.info("While creating new user getting UNAUTHORIZED response with guest credentials, this may state that our new rabbitMQ user is already created and we can skip this process.");
+            } else {
+                throw e;
+            }
         }
     }
 
     @Override
     public void disableGuestUser() {
 
+        log.info("Deleting the guest user");
+
         String disableGuestUserUrl = baseRabbitMqUrl + "/users/guest";
 
         HttpEntity<RabbitMQUser> disableGuestUserHttpEntity = new HttpEntity<>(null, getHttpHeader());
-        ResponseEntity<String> disableGuestUserResponse = restTemplate.exchange(
-                disableGuestUserUrl, HttpMethod.DELETE, disableGuestUserHttpEntity, String.class
-        );
 
-        if (disableGuestUserResponse.getStatusCode().is2xxSuccessful()) {
-            System.out.println("Guest User disabled successfully.");
-        } else {
-            System.err.println("Failed to disable guest user.");
+        try {
+            ResponseEntity<String> disableGuestUserResponse = restTemplate.exchange(
+                    disableGuestUserUrl, HttpMethod.DELETE, disableGuestUserHttpEntity, String.class
+            );
+
+            if (disableGuestUserResponse.getStatusCode().is2xxSuccessful()) {
+
+                log.info("Guest User deleted successfully.");
+            } else {
+                log.error("Failed to delete guest user, with response code : " + disableGuestUserResponse.getStatusCode() + " with message : " + disableGuestUserResponse.getBody());
+            }
+        } catch (HttpClientErrorException e) {
+
+            if (e.getStatusCode() == HttpStatus.NOT_FOUND) {
+                log.info("While deleting guest user getting NOT FOUND response, which states that guest user already deleted and we can skip this process");
+            }
         }
     }
 
